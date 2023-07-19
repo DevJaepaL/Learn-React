ed5098f60d1faa57a67b3edf529c5d16fc292d0b
DevJaepaL <wocks3254@gmail.com>
2023-07-19 20:45:32 +0900

    [Chapter 11] Component 최적화 ✔

--- react-ch10-todo-app/src/App.js
+++ react-ch10-todo-app/src/App.js
@@ -1,43 +1,64 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useReducer, useCallback, useRef, useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+// 랙 발생을 위한 많은 데이터 렌더링
+function createBulkTodo() {
+  const arr = [];
+
+  for (let i = 1; i <= 2500; i++) {
+    arr.push({
+      id: i,
+      todayText: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+
+  return arr;
+}
+
+// useReducer를 이용한 리렌더링 방지
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
   // 일정 항목에 대한 상태는 useState를 통해 관리한다.
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodo);
 
   // 객체의 고유값으로 사용될 ID, ref를 이용함.
-  const nextId = useRef(0);
-  const onInsert = useCallback(
-    (todayText) => {
-      const todo = {
-        id: nextId.current,
-        todayText,
-        checked: false,
-      };
+  const nextId = useRef(2501);
+  const onInsert = useCallback((todayText) => {
+    const todo = {
+      id: nextId.current,
+      todayText,
+      checked: false,
+    };
 
-      setTodos(todos.concat(todo));
-      nextId.current += 1; // 할 일이 추가되면 다음 ID 값 재 설정.
-    },
-    [todos],
-  );
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1; // 할 일이 추가되면 다음 ID 값 재 설정.
+  }, []);
 
   const onRemove = useCallback((id) => {
     // filter 함수를 통해 선택한 아이디를 삭제한 배열로 변환
-    setTodos(todos.filter((todo) => todo.id !== id));
-  });
+    dispatch({ type: 'REMOVE', id });
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
--- react-ch10-todo-app/src/components/TodoListItem.js
+++ react-ch10-todo-app/src/components/TodoListItem.js
@@ -5,21 +5,28 @@ import {
 } from 'react-icons/md';
 import './TodoListItem.scss';
 import cn from 'classnames';
+import React from 'react';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, todayText, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="todayText">{todayText}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="todayText">{todayText}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+/* 컴포넌트의 리렌더링 방지를 위한 memo 함수 사용. */
+export default React.memo(TodoListItem);
